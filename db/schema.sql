e8efbfe99ad8a0fbf5806e59add4a65556fd668e
AlexanderBanar <s595659@mail.ru>
2021-11-05 08:52:05 +0300

    Реализовать площадку продаж машин. [#4747 #222756] _ 2nd

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+create database selling;
+
+create table cars (
+                      id serial primary key,
+                      brand varchar,
+                      model varchar,
+                      transmission varchar,
+                      body varchar,
+                      engine varchar,
+                      year int,
+                      price int,
+                      status boolean
+);
+
+create table photos (
+                        id serial primary key,
+                        is_available boolean,
+                        path varchar
+);
+
+create table users (
+                       id serial primary key,
+                       name varchar
+);
